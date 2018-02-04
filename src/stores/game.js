57c3f24a00468c982f9aa5cca9323a3613a5756f
Adrien Lacroix <alacroix@users.noreply.github.com>
2018-02-04 15:28:16 +0100

    add winning condition (#10)
    * add game status in grid service
    
    * update game store for gameOver condition
    
    * use gameOver condition on grid and disable click if over
    
    * adapt restart button depending on the game status

--- src/stores/game.js
+++ src/stores/game.js
@@ -1,21 +1,28 @@
-import { action, extendObservable } from 'mobx';
+import { action, computed, extendObservable } from 'mobx';
 
-import { initGrid } from '../services/grid';
+import { initGrid, GAME_STATUS } from '../services/grid';
 
 class Game {
-  static getInitialState = () => ({
-    counter: 10,
-    grid: initGrid()
-  });
-
   constructor() {
-    extendObservable(this, Game.getInitialState());
+    extendObservable(this, {
+      counter: 10,
+      grid: initGrid(),
+      status: GAME_STATUS.RUNNING,
+      isGameOver: computed(() => this.status !== GAME_STATUS.RUNNING),
+      isWon: computed(() => this.status === GAME_STATUS.WON),
+      isLost: computed(() => this.status === GAME_STATUS.LOST)
+    });
   }
 
   restartGame = action(() => {
     this.grid = initGrid();
     this.counter = 10;
+    this.status = GAME_STATUS.RUNNING;
   });
+
+  changeStatus = newStatus => {
+    this.status = newStatus;
+  };
 
   incrementCounter = action(() => {
     this.counter += 1;
